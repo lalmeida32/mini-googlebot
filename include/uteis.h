21dbd024a923b1caaf83b0f3d1b55f76496a868b
Lucas Almeida <lucasf.almeida@hotmail.com>
2020-10-07 07:30:54 -0300

    Documentaçao do uteis.h adicionada

--- include/uteis.h
+++ include/uteis.h
@@ -3,8 +3,26 @@
 
     #include<stdio.h>
 
+    // Limpa o arquivo até '\n' ou fim de arquivo
+    // - Parâmetros
+    //    -> input: arquivo que será limpo
+    // - Retorno
+    //    -> não tem
     void flush(FILE *input);
+
+    // Lê inteiro de um arquivo e limpa até '\n' ou fim de arquivo
+    // - Parâmetros
+    //    -> input: arquivo de onde o inteiro será lido
+    // - Retorno
+    //    -> inteiro lido
     int ler_integer(FILE *input);
+
+    // Lê string até '\n', fim de arquivo ou comprimento máximo atingido
+    // - Parâmetros
+    //    -> length: comprimento máximo
+    //    -> input: arquivo de onde a string será lida 
+    // - Retorno
+    //    -> string lida (alocada dinamicamente na memória HEAP)
     char *ler_string(int length, FILE *input);
 
 #endif
