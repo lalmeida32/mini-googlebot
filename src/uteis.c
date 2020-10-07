c94e8279dd42486e8dcaabb5e34c56b9defa8450
Lucas Almeida <lucasf.almeida@hotmail.com>
2020-10-07 08:00:22 -0300

    documentacao de uteis.c adicionada

--- src/uteis.c
+++ src/uteis.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+// Limpa o arquivo até '\n' ou fim de arquivo
 void flush(FILE *input) {
     char c;
     do {
@@ -8,6 +9,7 @@ void flush(FILE *input) {
     } while(c != '\n' && !feof(input));
 }
 
+// Lê inteiro de um arquivo e limpa até '\n' ou fim de arquivo
 int ler_integer(FILE *input) {
     int integer;
 
@@ -17,13 +19,15 @@ int ler_integer(FILE *input) {
     return integer;
 }
 
+// Lê string até '\n', fim de arquivo ou comprimento máximo atingido
 char *ler_string(int length, FILE *input)
 {
-
+    // alocação de memória para string
     char *string = (char *)malloc(length * sizeof(char));
     int pos = 0;
     char c;
 
+    // ler até '\n', fim de arquivo ou o comprimento máximo ser atingido
     do
     {
         c = fgetc(input);
@@ -31,6 +35,9 @@ char *ler_string(int length, FILE *input)
     } while (c != '\n' && !feof(input) && pos < length);
     string[pos - 1] = '\0';
 
+    // limpando arquivo até '\n' ou fim de arquivo
+    // PS: função flush não funciona nesse caso, pois há necessidade de uma verificação
+    // inicial antes de entrar no loop
     while (c != '\n' && !feof(input))
     {
         c = fgetc(input);
