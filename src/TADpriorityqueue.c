8a4d6d0e66c63d73a3b863048ba4ce5f87c5c066
Lucas Almeida <lucasf.almeida@hotmail.com>
2020-12-17 17:33:57 -0300

    Adicionancomentários da inserção e remoção da pqueue

--- src/TADpriorityqueue.c
+++ src/TADpriorityqueue.c
@@ -92,14 +92,23 @@ void pqueue_free(PQUEUE **pqueue) {
 
 //funcao auxilixar da pqueue_inserir
 PQUEUE_NO *pqueue_inserir_recursiva(PQUEUE_NO *no_pai, int no_objetivo, int comprimento, void *dados, int (*dados_comparar) (void *, void *)) {
+    
+    // Chegou no nó destino
     if (comprimento == 0)
         return pqueue_no_criar(dados);
 
     const int constante_auxiliar = no_objetivo - (1 << comprimento);
+
+    // Comprimento até o nó objetivo diminui a cada filho da chamada
     const int comprimento_filho = comprimento - 1;
+
+    // Verifica se o próximo filho do percurso até o nó objetivo é o esquerdo ou direito
     const int seleciona_filho = constante_auxiliar >> comprimento_filho;
+
+    // Encontra o índice do nó objetivo para a sub-árvore do nó filho
     const int no_objetivo_filho = (constante_auxiliar) | (1 << comprimento_filho);
 
+    // Chamadas recursivas para encontrar o local do nó para inserir
     PQUEUE_NO *no_filho;
     if (seleciona_filho) {
         no_filho = pqueue_inserir_recursiva(no_pai->direita, no_objetivo_filho, comprimento_filho, dados, dados_comparar);
@@ -110,6 +119,7 @@ PQUEUE_NO *pqueue_inserir_recursiva(PQUEUE_NO *no_pai, int no_objetivo, int comp
         no_pai->esquerda = no_filho;
     }
 
+    // Fix-up
     if (dados_comparar(no_filho->dados, no_pai->dados) > 0) {
         void *dados_temporario = no_filho->dados;
         no_filho->dados = no_pai->dados;
@@ -119,30 +129,47 @@ PQUEUE_NO *pqueue_inserir_recursiva(PQUEUE_NO *no_pai, int no_objetivo, int comp
     return no_pai;
 }
 
+// Insere nó na max heap que é utilizada para a priority queue
 void pqueue_inserir(PQUEUE *pqueue, void *dados) {
     if (!pqueue || !pqueue->dados_comparar) return;
 
+    // Nó objetivo está no índice "quantidade + 1" pois, estamos considerando os índices começando em "1"
     const int no_objetivo = pqueue->quantidade + 1;
+    // Captura altura da árvore com a inserção do novo nó
     const int comprimento_ate_no_objetivo = pqueue->altura + !(no_objetivo & pqueue->quantidade);
+
+    // Insere o novo nó
     pqueue->raiz = pqueue_inserir_recursiva(pqueue->raiz, no_objetivo, comprimento_ate_no_objetivo, dados, pqueue->dados_comparar);
 
+    // Atualiza a quantidade e a altura da pqueue
     pqueue->quantidade = no_objetivo;
     pqueue->altura = comprimento_ate_no_objetivo;
 }
 
+// Função auxiliar da pqueue_remover
 PQUEUE_NO *pqueue_remover_recursiva(PQUEUE_NO *no_pai, int no_objetivo, int comprimento, PQUEUE_NO *raiz, void (*dados_apagar) (void **)) {
 
+    // Chegou no nó destino
     if (comprimento == 0) {
+        // Troca com nó raiz e remove
         raiz->dados = no_pai->dados;
         pqueue_no_free(&no_pai, dados_apagar);
         return NULL;
     }
 
     const int constante_auxiliar = no_objetivo - (1 << comprimento);
+
+    // Comprimento até o nó objetivo diminui a cada filho da chamada
     const int comprimento_filho = comprimento - 1;
+
+    // Verifica se o próximo filho do percurso até o nó objetivo é o esquerdo ou direito
     const int seleciona_filho = constante_auxiliar >> comprimento_filho;
+
+    // Encontra o índice do nó objetivo para a sub-árvore do nó filho
     const int no_objetivo_filho = (constante_auxiliar) | (1 << comprimento_filho);
 
+
+    // Chamadas recursivas para encontrar o nó_objetivo
     PQUEUE_NO *no_filho;
     if (seleciona_filho) {
         no_filho = pqueue_remover_recursiva(no_pai->direita, no_objetivo_filho, comprimento_filho, raiz, dados_apagar);
@@ -167,6 +194,8 @@ void pqueue_fix_down(PQUEUE_NO *no_pai, int (*dados_comparar) (void *, void *)) 
     if (direita && dados_comparar(troca->dados, direita->dados) < 0)
         troca = direita;
     
+    // Se tiver algum filho maior que o pai e maior que o outro filho,
+    // troca os dados de pai e filho e chama recursivamente pro próximo filho
     if (troca != no_pai) {
         void *dados_temporario = troca->dados;
         troca->dados = no_pai->dados;
@@ -177,19 +206,23 @@ void pqueue_fix_down(PQUEUE_NO *no_pai, int (*dados_comparar) (void *, void *)) 
 
 //remove o no (do topo) fila de prioridade
 void pqueue_remover(PQUEUE *pqueue) {
+
     if (!pqueue || pqueue->quantidade == 0 || !pqueue->dados_comparar) return;
 
+    // Nó objetivo é o último pois será o nó a ter os dados trocados com a raiz e a ser removido
     const int no_objetivo = pqueue->quantidade;
     const int comprimento_ate_no_objetivo = pqueue->altura;
 
+    // Troca os dados do último nó com os do nó raiz e remove último nó
     pqueue->raiz = pqueue_remover_recursiva(pqueue->raiz, no_objetivo, comprimento_ate_no_objetivo, pqueue->raiz, pqueue->dados_apagar);
 
+    // Cálculo para quantidade e altura serem atualizadas
     pqueue->quantidade = no_objetivo - 1;
     pqueue->altura = comprimento_ate_no_objetivo - !(no_objetivo & pqueue->quantidade);
 
     if (pqueue->quantidade == 0) return;
     
-    //estaurando a ordem da heap (max heap)
+    //restaurando a ordem da heap (max heap)
     pqueue_fix_down(pqueue->raiz, pqueue->dados_comparar);
 
 }
